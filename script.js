25637efac001dc35a6d351b510b71ce9f1360e82
Matheus <matheus.frndes@gmail.com>
2016-04-02 11:59:44 -0300

    Add terminal style animation for "soon" messages

--- script.js
+++ script.js
@@ -3,5 +3,28 @@
  */
 
 $(function () {
-    $('#name').delay(150).animate({fontSize: '5em'});
+    var soon = $('#soon');
+    var strings = ['em breve', 'spoedig', 'pronto', 'скоро', '不久', 'すぐに', 'již brzy', 'primum', 'bald', 'presto'];
+
+    for (var i = strings.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = strings[i];
+        strings[i] = strings[j];
+        strings[j] = temp;
+    }
+
+    strings.unshift('soon');
+
+    strings = strings.map(function (e) {
+        return e + " ^1000"
+    });
+
+    setTimeout(function () {
+        $(soon).typed({
+            strings: strings,
+            typeSpeed: 75,
+            backSpeed: 25,
+            loop: true
+        });
+    }, 1000);
 });
