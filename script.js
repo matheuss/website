8d2975059041116710c0c8cb2c6ca53dbe6f8031
Matheus <matheus.frndes@gmail.com>
2016-04-02 02:18:41 -0300

    Code style fix

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
  * Created by matheus on 4/2/16.
  */
 
-$(function() {
-    $('#name').delay(150).animate({ fontSize : '5em' });
+$(function () {
+    $('#name').delay(150).animate({fontSize: '5em'});
 });
