79c62922ef08959686ac90c91179f12e584db378
Matheus Fernandes <github@matheus.top>
2016-07-31 14:09:01 -0300

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const babel = require('gulp-babel');
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const st = require('st');
+const uglify = require('gulp-uglify');
+const livereload = require('gulp-livereload');
+
+gulp.task('build', () => {
+	gulp.src('src/*.pug').pipe(pug()).pipe(gulp.dest('./')).pipe(livereload());
+	gulp.src('src/*.css').pipe(gulp.dest('dist')).pipe(livereload());
+	gulp.src('src/*.js')
+		.pipe(babel({presets: ['es2015']}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'))
+		.pipe(livereload());
+});
+
+gulp.task('watch', () => {
+	http.createServer(st({path: process.cwd(), index: 'index.html', cache: false})).listen(3000);
+	livereload.listen();
+	gulp.watch(['src/*'], ['build']);
+});
