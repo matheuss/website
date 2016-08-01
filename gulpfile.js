e21c5c39de14636cd065012939f476f8ca98618b
Matheus Fernandes <github@matheus.top>
2016-08-01 00:54:21 -0300

    Add Stylus

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,17 @@ const babel = require('gulp-babel');
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const st = require('st');
+const stylus = require('gulp-stylus');
 const uglify = require('gulp-uglify');
 const livereload = require('gulp-livereload');
 
 gulp.task('build', () => {
 	gulp.src('assets/*').pipe(gulp.dest('./')).pipe(livereload());
 	gulp.src('src/*.pug').pipe(pug()).pipe(gulp.dest('./')).pipe(livereload());
-	gulp.src('src/*.css').pipe(gulp.dest('dist')).pipe(livereload());
+	gulp.src('src/*.styl')
+		.pipe(stylus({compress: true}))
+		.pipe(gulp.dest('dist'))
+		.pipe(livereload());
 	gulp.src('src/*.js')
 		.pipe(babel({presets: ['es2015']}))
 		.pipe(uglify())
