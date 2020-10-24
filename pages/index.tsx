5d96f19ea6ee13f74aad5129ada0fad1bea43059
Matheus Fernandes <matheus.frndes@gmail.com>
2020-10-24 04:34:49 +0200

    Remove br – it's no longer needed

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,6 @@ const Index = () => {
                     <h1 id='name' onClick={handleClick}>Matheus</h1>
                     <h2 id='title'>Head of Infra, <Link href="https://vercel.com">Vercel</Link></h2>
                 </div >
-                <br />
                 <p id="social">
                     <Link href="https://twitter.com/matheusfrndes">Twitter</Link> |{' '}
                     <Link href="https://www.linkedin.com/in/matheusfrndes">LinkedIn</Link> |{' '}
