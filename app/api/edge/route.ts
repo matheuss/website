7e35a38de73b7d1db82b0f3f3fab47c9fef09154
Matheus Fernandes <matheus.frndes@gmail.com>
2024-07-11 10:59:10 +0000

    Revert "Remove lambda"
    This reverts commit 5ece693cee3878b366838e02d08e77b1c10c9c1f.

---
+++ app/api/edge/route.ts
@@ -0,0 +1,9 @@
+export function GET(request: Request) {
+  return new Response(
+    JSON.stringify(Object.fromEntries(request.headers.entries()), null, 2),
+    {
+      status: 200,
+      headers: { "content-type": "text/plain" },
+    }
+  );
+}
