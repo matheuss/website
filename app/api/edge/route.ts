5ece693cee3878b366838e02d08e77b1c10c9c1f
Matheus Fernandes <matheus.frndes@gmail.com>
2024-07-11 10:27:19 +0000

    Remove lambda

--- app/api/edge/route.ts
+++
@@ -1,9 +0,0 @@
-export function GET(request: Request) {
-  return new Response(
-    JSON.stringify(Object.fromEntries(request.headers.entries()), null, 2),
-    {
-      status: 200,
-      headers: { "content-type": "text/plain" },
-    }
-  );
-}
